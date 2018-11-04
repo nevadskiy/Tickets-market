0fb1a9a9660e6cfb2e1b92df803262716b6298cd
Vitalii Kalaida <nevadskiy@gmail.com>
2018-11-04 21:14:08 +0200

    Feature concert card view

--- resources/views/concerts/show.blade.php
+++ resources/views/concerts/show.blade.php
@@ -1,34 +1,27 @@
-<h1>{{ $concert->title }}</h1>
-<h2>{{ $concert->subtitle }}</h2>
-<p>{{ $concert->formatted_date }}</p>
-<p>Doors at {{ $concert->formatted_start_time }}</p>
-<p>{{ $concert->ticket_price_in_dollars }}</p>
-<p>{{ $concert->venue }}</p>
-<p>{{ $concert->venue_address }}</p>
-<p>{{ $concert->city }}, {{ $concert->state }} {{ $concert->zip }}</p>
-<p>{{ $concert->additional_information }}</p>
+@extends('layouts.master')
 
-{{--<!doctype html>--}}
-{{--<html lang="{{ app()->getLocale() }}">--}}
-{{--<head>--}}
-    {{--<meta charset="utf-8">--}}
-    {{--<meta http-equiv="X-UA-Compatible" content="IE=edge">--}}
-    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
-
-    {{--<title>Laravel</title>--}}
-
-    {{--<!-- Fonts -->--}}
-    {{--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">--}}
-
-    {{--<link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
-{{--</head>--}}
-{{--<body>--}}
-    {{--<div class="container">--}}
-        {{--<div class="card">--}}
-            {{--<div class="card-header">--}}
-                {{--<h1>{{ $concert->title }}</h1>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</body>--}}
-{{--</html>--}}
+@section('body')
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>{{ $concert->title }}</h1>
+                        <h2>{{ $concert->subtitle }}</h2>
+                    </div>
+                    <div class="card-body">
+                        <p>{{ $concert->formatted_date }}</p>
+                        <p>Doors at {{ $concert->formatted_start_time }}</p>
+                        <p>{{ $concert->ticket_price_in_dollars }}</p>
+                        <p>{{ $concert->venue }}</p>
+                        <p>{{ $concert->venue_address }}</p>
+                        <p>{{ $concert->city }}, {{ $concert->state }} {{ $concert->zip }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <p>{{ $concert->additional_information }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
