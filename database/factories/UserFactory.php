f9d041761aa79990a8c13a24dad1ed75b5b67ce0
Vitalii Kalaida <nevadskiy@gmail.com>
2018-12-04 23:02:33 +0200

    Refactor UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-        'stripe_account_id' => 'test_stripe_account_1234'
+        'stripe_account_id' => 'test_stripe_account_1234',
+        'stripe_access_token' => 'test_token',
     ];
 });
